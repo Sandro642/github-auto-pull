654dd8faa7d688ba13f23b5f2b16e8422af7640b
Sandro Soria <sandro33810@gmail.com>
2024-02-22 16:23:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const { AutoPull } = require('./util')
 
-new AutoPull("Skym-Mc", "SkymClient", "Desktop", 5).withBranch("skymclientv2").withExec("npm i").oauth("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").start()
+new AutoPull("Skym-Mc", "SkymClient", "Desktop", 5).withBranch("skymclientv2").withExec("npm i").oauth("").start()
