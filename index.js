356f4150bc64eac3241ebc47a11750856773917d
Sandro Soria <sandro33810@gmail.com>
2024-02-22 12:27:33 +0100

    Mise à jour de la classe AutoPull avec des modifications de code

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
 const { AutoPull } = require('./util')
 
-/*
-EXAMPLES
-
-########## FOR PRIVATE REPOSITORIES ##########
-
-new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds), function() {...} (execute code when files are updated)).auth("username", "password").start()
-new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds), function() {...} (execute code when files are updated)).oauth("oauthtoken").start()
-
-new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds), function() {...} (execute code when files are updated)).auth("username", "password").withBranch("branchName").start()
-new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds), function() {...} (execute code when files are updated)).oauth("oauthtoken").withBranch("branchName").start()
-
-########## FOR PUBLIC REPOSITORIES ##########
-
-new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds), function() {...} (execute code when files are updated)).start()
-new AutoPull("repoAccount", "repoName", "projectPath", int pullInterval (in seconds), function() {...} (execute code when files are updated)).withBranch("branchName").start()
-
-########## STOP ##########
-
-const pull = new AutoPull(...)
-pull.stop()
-
-########## EXECUTE CODE ##########
-
-new AutoPull(...).withExec("command").start()
-
-########## DISABLE GIT LOG ##########
-
-new AutoPull(...).disableGitLogs()
-
-You can also use global variables for authentication, but i let you do that yourself :)
-*/
-
-new AutoPull("MohistMC", "Mohist", "path", 5).withBranch("1.16.5").start()
+new AutoPull("Skym-Mc", "SkymClient", "Desktop", 5).withBranch("skymclientv2").withExec("npm i").oauth("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").start()
--- util.js
+++ util.js
@@ -1,61 +1,61 @@
-const fs = require('fs')
-const execSync = require('child_process').execSync
-const path = require('path')
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+const path = require('path');
 
 class AutoPull {
     constructor(repoAccount, repoName, projectPath, pullInterval, functionToExecute) {
         if (!repoAccount || !repoName || !projectPath || !pullInterval)
             log("You have to specify repo account, repo name, project path and the pull interval.");
-        this.repoAccount = repoAccount
-        this.repoName = repoName
-        this.projectPath = projectPath
-        this.pullInterval = pullInterval
-        this.branch = ''
-        this.url = ''
-        this.gitlog = true
-        this.functionToExecute = functionToExecute
+        this.repoAccount = repoAccount;
+        this.repoName = repoName;
+        this.projectPath = projectPath;
+        this.pullInterval = pullInterval;
+        this.branch = '';
+        this.url = '';
+        this.gitlog = true;
+        this.functionToExecute = functionToExecute;
         this.command = ''; // Variable pour stocker la commande
-        return this
+        return this;
     }
 
     oauth(token) {
         if (!token) {
             log("You have to specify username and password.", this);
-            return
+            return;
         }
-        this.url = '"https://' + token + '@github.com/' + this.repoAccount + '/' + this.repoName + '.git"'
-        return this
+        this.url = '"https://' + token + '@github.com/' + this.repoAccount + '/' + this.repoName + '.git"';
+        return this;
     }
 
     auth(username, password) {
         if (!username || !password) {
             log("You have to specify username and password.", this);
-            return
+            return;
         }
-        this.url = '"https://' + username + ':' + password + '@github.com/' + this.repoAccount + '/' + this.repoName + '.git"'
-        return this
+        this.url = '"https://' + username + ':' + password + '@github.com/' + this.repoAccount + '/' + this.repoName + '.git"';
+        return this;
     }
 
     withBranch(branch) {
-        this.branch = " -b " + branch
-        return this
+        this.branch = " -b " + branch;
+        return this;
     }
 
     disableGitLogs() {
-        this.gitlog = false
-        return this
+        this.gitlog = false;
+        return this;
     }
 
     withExec(command) {
         this.command = command; // Définir la commande à exécuter
-        return this
+        return this;
     }
 
     start() {
         if (!this.repoAccount || !this.repoName || !this.projectPath || !this.pullInterval) return this;
-        if (this.url === '') this.url = '"https://github.com/' + this.repoAccount + '/' + this.repoName + '.git"'
+        if (this.url === '') this.url = '"https://github.com/' + this.repoAccount + '/' + this.repoName + '.git"';
 
-        log('Started auto pull for repo https://github.com/' + this.repoAccount + '/' + this.repoName, this)
+        log('Started auto pull for repo https://github.com/' + this.repoAccount + '/' + this.repoName, this);
 
         // Vérification et création du dossier si nécessaire
         if (!fs.existsSync(this.projectPath)) {
@@ -91,49 +91,55 @@ class AutoPull {
         }
 
         return this;
-    }   
+    }
 
     stop() {
-        clearInterval(this.interval)
-        log('Stopped auto pull for repo https://github.com/' + this.repoAccount + '/' + this.repoName, this)
-        return this
+        clearInterval(this.interval);
+        log('Stopped auto pull for repo https://github.com/' + this.repoAccount + '/' + this.repoName, this);
+        return this;
     }
 
     pull() {
         log('Pulling...', this);
-
-        // Vérifier et exécuter la commande définie (si elle existe)
-        if (this.command) {
-            execSync(this.command, {
-                cwd: this.projectPath
-            });
-        }
-
+    
         execSync('git reset --hard', {
             cwd: this.projectPath
-        })
-
+        });
+    
         const pullOut = execSync('git pull ' + this.url, {
             cwd: this.projectPath,
             stdio: 'pipe'
-        }).toString()
-
+        }).toString();
+    
         if (this.gitlog) log(pullOut, this);
+    
+        if (pullOut.includes('Already up to date.')) {
+            
+            log('Already up to date.', this);
 
-        if (!pullOut.includes('Already up to date.')) {
-            //Do an action here :)
-            //execSync('pm2 restart 0')
-            if(this.functionToExecute) this.functionToExecute()
-        }
+        } else {
+            
+            log('Pulled successfully.', this);
+
+            if (this.command) {
+                execSync(this.command, {
+                    cwd: this.projectPath
+                });
+                log('Command executed successfully after the pull.', this);
+            }
+        }        
+
+        
     }
+    
 }
 
 function log(msg, cl) {
-    return console.log('[' + new Date().toLocaleString() + (!cl ? '' : ' | ' + cl.repoAccount + '/' + cl.repoName) + ']', msg)
+    return console.log('[' + new Date().toLocaleString() + (!cl ? '' : ' | ' + cl.repoAccount + '/' + cl.repoName) + ']', msg);
 }
 
 function isDirEmpty(dirname) {
-    return fs.readdirSync(dirname).length === 0
+    return fs.readdirSync(dirname).length === 0;
 }
 
-module.exports.AutoPull = AutoPull
+module.exports.AutoPull = AutoPull;
