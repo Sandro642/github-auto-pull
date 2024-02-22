dcee506929d78d7bf74162643a7d5ba3fa5da06e
Sandro Soria <sandro33810@gmail.com>
2024-02-22 16:20:53 +0100

    Mise à jour de AutoPull pour prendre en charge les jetons d'authentification GitHub

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const { AutoPull } = require('./util')
 
-new AutoPull("Skym-Mc", "SkymClient", "Desktop", 5).withBranch("skymclientv2").withExec("npm i").oauth("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").start()
+new AutoPull("Skym-Mc", "SkymClient", "Desktop", 5).withBranch("skymclientv2").withExec("npm i").oauth("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").start()
